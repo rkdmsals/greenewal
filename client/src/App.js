2592e336a9693ecaade06264471f6be47b62533e
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-11-05 23:19:29 +0900

    만점고사 페이지 1차

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./components/utilis/variables.css"
 import Home from "./components/views/Home";
 import SpeakerPage from "./components/views/SpeakerPage/SpeakerPage";
+import TestStart from "./components/views/game/TestStart";
+import Test from "./components/views/game/Test"
+import ProductDisplay from "./components/views/ShoppingPage/ProductDisplay";
+import Menu from "./components/views/menu/menu";
 
 function App() {
   return (
@@ -11,6 +15,9 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/shop" element={<ProductDisplay />} />
           <Route exact path="/speaker" element={<SpeakerPage />} />
+          <Route exact path="/testStart" element={<TestStart />} />
+          <Route exact path="/test" element={<Test />} />
+          <Route exact path="/menu" element={<Menu />} />
         </Routes>
       </BrowserRouter>
     </div>
