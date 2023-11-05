55c87e3744641610c8edfc0bbe3c49468e135e9d
MK-cyb <mindy50@ewhain.net>
2023-11-05 22:14:53 +0900

    강연정보 페이지 1차

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./components/utilis/variables.css"
 import Home from "./components/views/Home";
+import SpeakerPage from "./components/views/SpeakerPage/SpeakerPage";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/speaker" element={<SpeakerPage />} />
         </Routes>
       </BrowserRouter>
     </div>
