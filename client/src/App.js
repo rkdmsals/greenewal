285cce4e7fae2f5779eafc4dc61ecadda03bbe36
rkdmsslasla <gaeun9477@gmail.com>
2023-11-10 16:32:09 +0900

    ShoppingCart 페이지 추가

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./components/utilis/variables.css"
 import Home from "./components/views/Home";
 import SpeakerPage from "./components/views/SpeakerPage/SpeakerPage";
+import ProductDisplay from "./components/views/ShoppingPage/ProductDisplay";
+import ShoppingCart from "./components/views/ShoppingPage/ShoppingCart";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/shop" element={<ProductDisplay />} />
+          <Route exact path="/cart" element={<ShoppingCart />} />
           <Route exact path="/speaker" element={<SpeakerPage />} />
+
         </Routes>
       </BrowserRouter>
     </div>
