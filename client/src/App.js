f5925ea583f7a1bb5870c4e5c6a78c7f9bc26781
JEES23 <76464398+JEES23@users.noreply.github.com>
2023-11-13 02:57:11 +0900

    로그인,회원가입 프론트 완료

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,25 @@ import SpeakerPage from "./components/views/SpeakerPage/SpeakerPage";
 import ProductDisplay from "./components/views/ShoppingPage/ProductDisplay";
 import ShoppingCart from "./components/views/ShoppingPage/ShoppingCart";
 import LoginPage from "./components/views/Login/LoginPage";
+import RegisterPage from "./components/views/Register/RegisterPage";
 import TestStart from "./components/views/game/TestStart";
 import Test from "./components/views/game/Test"
 import Menu from "./components/views/menu/menu";
+import Auth from "./hoc/auth";
 
 function App() {
+
+  
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home /> } />
           <Route exact path="/shop" element={<ProductDisplay />} />
           <Route exact path="/cart" element={<ShoppingCart />} />
           <Route exact path="/speaker" element={<SpeakerPage />} />
           <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/register" element={<RegisterPage />} />
           <Route exact path="/testStart" element={<TestStart />} />
           <Route exact path="/test" element={<Test />} />
           <Route exact path="/menu" element={<Menu />} />
