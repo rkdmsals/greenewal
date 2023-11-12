952c46803719357cb9f485fa81447f445d15d340
MK-cyb <mindy50@ewhain.net>
2023-11-13 00:24:39 +0900

    speaker page 2차

--- client/src/components/views/SpeakerPage/SpeakerPage.js
+++ client/src/components/views/SpeakerPage/SpeakerPage.js
@@ -1,7 +1,9 @@
 import "./SpeakerPage.css";
 import React from 'react';
 import { ReactComponent as Dia } from "../../../assets/images/SpeakerPage/dia.svg";
-import { ReactComponent as Speaker1 } from "../../../assets/images/SpeakerPage/speaker1.svg";
+import { ReactComponent as Speaker_1 } from "../../../assets/images/SpeakerPage/speaker1.svg";
+import { ReactComponent as Speaker_2 } from "../../../assets/images/SpeakerPage/speaker2.svg";
+
 
 function SpeakerPage() {
     return (
@@ -60,25 +62,25 @@ function SpeakerPage() {
               <div className="x_scroll">
                 <div className="speakerInfo" id="speakerInfo_1">
                     <div className="speakerInfo_content">
-                        <div className="speakerInfo_name">이주미</div>
+                        <div className="speakerInfo_name speakerInfo_name_1">이주미</div>
                         <div className="speakerInfo_topic_content">강연주제</div>
-                        <div className="speaker_svg"><Speaker1 /></div>
+                        <div className="speaker_svg1"><Speaker_1 /></div>
                     </div>
                     
                 </div>
                 <div className="speakerInfo" id="speakerInfo_2">
                     <div className="speakerInfo_content">
-                        <div className="speakerInfo_name">이주미</div>
-                        <div className="speakerInfo_topic_content">강연주제</div>
-                        <div className="speaker_svg"><Speaker1 /></div>
+                        <div className="speakerInfo_name speakerInfo_name_2">전효진</div>
+                        <div className="speakerInfo_topic_content speakerInfo_topic_content_2">강연주제</div>
+                        <div className="speaker_svg2"><Speaker_2 /></div>
                     </div>
                     
                 </div>
                 <div className="speakerInfo" id="speakerInfo_3">
                     <div className="speakerInfo_content">
-                        <div className="speakerInfo_name">이주미</div>
+                        <div className="speakerInfo_name speakerInfo_name_3">이주미</div>
                         <div className="speakerInfo_topic_content">강연주제</div>
-                        <div className="speaker_svg"><Speaker1 /></div>
+                        <div className="speaker_svg3"><Speaker_2 /></div>
                     </div>
                     
                 </div>
