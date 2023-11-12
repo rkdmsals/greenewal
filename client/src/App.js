278c04cf7cf90cb55bdac01c6c0d26e37b553127
JEES23 <76464398+JEES23@users.noreply.github.com>
2023-11-12 20:37:16 +0900

    로그인 프론트엔드 수정

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,7 @@ import Home from "./components/views/Home";
 import SpeakerPage from "./components/views/SpeakerPage/SpeakerPage";
 import ProductDisplay from "./components/views/ShoppingPage/ProductDisplay";
 import ShoppingCart from "./components/views/ShoppingPage/ShoppingCart";
-import LoginButton from "./components/views/LoginButton";
-import Login from "./components/Login";
+import LoginPage from "./components/views/LoginPage";
 import TestStart from "./components/views/game/TestStart";
 import Test from "./components/views/game/Test"
 import Menu from "./components/views/menu/menu";
@@ -19,14 +18,7 @@ function App() {
           <Route exact path="/shop" element={<ProductDisplay />} />
           <Route exact path="/cart" element={<ShoppingCart />} />
           <Route exact path="/speaker" element={<SpeakerPage />} />
-          <Route
-            exact path="/login"
-            element={<LoginButton />}
-          />
-          <Route
-            path="redirect"
-            element={<Login />}
-          />
+          <Route exact path="/login" element={<LoginPage />} />
           <Route exact path="/testStart" element={<TestStart />} />
           <Route exact path="/test" element={<Test />} />
           <Route exact path="/menu" element={<Menu />} />
---
+++ client/src/components/views/Login/LoginPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function LoginPage() {
+    return (
+   <div> login </div>
+    )
+}
+export default LoginPage;
