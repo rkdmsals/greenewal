01beab61350f510f8baff9c25fae37856db356aa
MK-cyb <mindy50@ewhain.net>
2023-11-13 01:08:25 +0900

    speaker detail page 1차

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./components/utilis/variables.css"
 import Home from "./components/views/Home";
 import SpeakerPage from "./components/views/SpeakerPage/SpeakerPage";
+import SpeakerDetailPage from "./components/views/SpeakerPage/SpeakerDetailPage/SpeakerDeatailPage";
 import ProductDisplay from "./components/views/ShoppingPage/ProductDisplay";
 import ShoppingCart from "./components/views/ShoppingPage/ShoppingCart";
 import LoginPage from "./components/views/Login/LoginPage";
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/shop" element={<ProductDisplay />} />
           <Route exact path="/cart" element={<ShoppingCart />} />
           <Route exact path="/speaker" element={<SpeakerPage />} />
+          <Route exact path="/speakerDetail" element={<SpeakerDetailPage />} />
           <Route exact path="/login" element={<LoginPage />} />
           <Route exact path="/testStart" element={<TestStart />} />
           <Route exact path="/test" element={<Test />} />
