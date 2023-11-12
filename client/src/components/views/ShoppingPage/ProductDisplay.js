69c160e7cf91cd2d57c3c4fe34c8a19dbb37da5a
rkdmsslasla <gaeun9477@gmail.com>
2023-11-13 01:04:00 +0900

    ProductDisplay 3차

--- client/src/components/views/ShoppingPage/ProductDisplay.js
+++ client/src/components/views/ShoppingPage/ProductDisplay.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import './ProductDisplay.css';
 
 function ProductDisplay() {
+    const navigate = useNavigate();
 
     const detailImg_1 = [
         "../img/goods/goods_detail/1-2.png",
@@ -72,7 +73,7 @@ function ProductDisplay() {
         {
             id: 3,
             title: '말랑비즈인형',
-            originalprice: 13000,
+            originalPrice: 13000,
             discountedPrice: 10000,
             describe: '스트레스 받을 땐 말랑콩떡 버섯 인형을 꾹꾹 눌러봐요!',
             img: "../img/goods/goods_detail/3-1.png",
@@ -81,7 +82,7 @@ function ProductDisplay() {
         {
             id: 4,
             title: '액정클리너',
-            originalprice: 2000,
+            originalPrice: 2000,
             discountedPrice: 1500,
             describe: '언제 어디서든 화면이 흐릿하면 이화이언 클리너로 쓱싹!',
             img: "../img/goods/goods_detail/4-1.png",
@@ -90,7 +91,7 @@ function ProductDisplay() {
         {
             id: 5,
             title: '레고 피규어',
-            originalprice: 11000,
+            originalPrice: 11000,
             discountedPrice: 9000,
             describe: '초록 후드의 캐릭터, 공구템을 입고 있는 벗들의 모습 같지 않나요?',
             img: "../img/goods/goods_detail/5-1.png",
@@ -99,7 +100,7 @@ function ProductDisplay() {
         {
             id: 6,
             title: '그립톡',
-            originalprice: 4000,
+            originalPrice: 4000,
             discountedPrice: 3000,
             describe: '달콤한 바닐라맛의 아이스크림 그립톡, 보기만 해도 놀이동산이 가고 싶어지지 않나요?',
             img: "../img/goods/goods_detail/6-1.png",
@@ -135,7 +136,7 @@ function ProductDisplay() {
         {
             id: 3,
             title: '말랑비즈인형',
-            originalprice: 13000,
+            originalPrice: 13000,
             discountedPrice: 10000,
             describe: '스트레스 받을 땐 말랑콩떡 버섯 인형을 꾹꾹 눌러봐요!',
             img: "../img/goods/goods_detail/3-1.png",
@@ -149,7 +150,7 @@ function ProductDisplay() {
             {
                 id: 5,
                 title: '레고 피규어',
-                originalprice: 11000,
+                originalPrice: 11000,
                 discountedPrice: 9000,
                 describe: '초록 후드의 캐릭터, 공구템을 입고 있는 벗들의 모습 같지 않나요?',
                 img: "../img/goods/goods_detail/5-1.png",
@@ -158,7 +159,7 @@ function ProductDisplay() {
             {
                 id: 6,
                 title: '그립톡',
-                originalprice: 4000,
+                originalPrice: 4000,
                 discountedPrice: 3000,
                 describe: '달콤한 바닐라맛의 아이스크림 그립톡, 보기만 해도 놀이동산이 가고 싶어지지 않나요?',
                 img: "../img/goods/goods_detail/6-1.png",
@@ -173,32 +174,44 @@ function ProductDisplay() {
         <div className="ProductDisplay_background">
             <div className="ProductDisplay_main_contianer">
                 <div className="ProductDisplay_main_title">
-                    Previous Goods
+                    Previous
+                    <br />
+                    Goods
                 </div>
                 <div className="ProductDisplay_top_button_container">
                     <button className="ProductDisplay_top_button1" onClick={() => handleButton1Click("버튼 1")}>전체</button>
                     <button className="ProductDisplay_top_button2" onClick={() => handleButton2Click("버튼 2")}>2022 배꽃정원</button>
                     <button className="ProductDisplay_top_button3" onClick={() => handleButton3Click("버튼 3")}>2023 배꽃정원</button>
-
                 </div>
                 <div className="ProductDisplay_inner_container">
                     {currentDisplay.map((product) => (
                         <div key={product.id} className="ProductDisplay_content_container">
-                            <div className='ProductDisplay_content_title'>
-                                {product.title}
+                            <div className="ProductDisplay_content_left_container">
+                                <div className='ProductDisplay_content_title'>
+                                    {product.title}
+                                </div>
+                                <div className="ProductDisplay_content_price_container">
+                                    <div className='ProductDisplay_content_original_price'>
+                                        {product.originalPrice}￦
+                                    </div>
+                                    <div className='ProductDisplay_content_discounted_price'>
+                                        {product.discountedPrice}￦
+                                    </div>
+                                </div>
                             </div>
-                            <div className='ProductDisplay_content_price'>
-                                가격: {product.discountedPrice}￦
-                            </div>
-                            <div className='ProductDisplay_content_describe'>
-                                {product.describe}
+
+                            <div className="ProductDisplay_content_right_container">
+
                             </div>
                         </div>
                     ))}
                 </div>
-                <button className="ProductDisplay_cart_button">
-                    장바구니 가기
-                </button>
+                <div className="ProductDisplay_cart_button_container">
+                    <button className="ProductDisplay_cart_button" onClick={() => navigate('/ShoppingCart')}>
+                        장바구니 가기
+                    </button>
+                </div>
+
             </div>
         </div >)
 }
