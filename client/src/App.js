f3c593fbf2d6f1062d4b1a6a6877deaea83b686a
rkdmsslasla <gaeun9477@gmail.com>
2023-11-14 00:53:44 +0900

    임시 commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./components/utilis/variables.css"
 import Home from "./components/views/Home";
 import SpeakerPage from "./components/views/SpeakerPage/SpeakerPage";
 import ProductDisplay from "./components/views/ShoppingPage/ProductDisplay";
+import ProductDetail from "./components/views/ShoppingPage/ProductDetail";
 import ShoppingCart from "./components/views/ShoppingPage/ShoppingCart";
 import LoginPage from "./components/views/Login/LoginPage";
 import TestStart from "./components/views/game/TestStart";
 import Test from "./components/views/game/Test"
 import Menu from "./components/views/menu/menu";
+
 
 function App() {
   return (
@@ -16,6 +19,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/shop" element={<ProductDisplay />} />
+          <Route path="/shop/product-detail/:productId" element={<ProductDetail />} />
           <Route exact path="/cart" element={<ShoppingCart />} />
           <Route exact path="/speaker" element={<SpeakerPage />} />
           <Route exact path="/login" element={<LoginPage />} />
