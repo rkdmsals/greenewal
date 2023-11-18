1f5a58eba8998be0b78cd4f4ca83dbb29a59cc15
JEES23 <76464398+JEES23@users.noreply.github.com>
2023-11-19 04:35:19 +0900

    사이드바, 티켓확인 1차

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./components/utilis/variables.css"
 import Home from "./components/views/Home";
@@ -14,16 +14,25 @@ import TestStart from "./components/views/game/TestStart";
 import Test from "./components/views/game/Test"
 import CheckAnswers from "./components/views/game/CheckAnswers";
 import Menu from "./components/views/menu/menu";
-import Auth from "./hoc/auth";
-
+import SideBar from "./components/Others/SideBar";
+import Ticket from "./components/views/Ticket/TicketPage"
 
 function App() {
 
+  const [showSideBar, setShowSideBar ] = useState(false)
 
   return (
     <div className="App">
+
+    <img src="img/menu_navigation.png" alt='sidebaricon' className="sidebaricon" 
+        onClick={() => setShowSideBar(!showSideBar)}
+        />
       <BrowserRouter>
+
+      
         <Routes>
+
+          
           <Route exact path="/" element={<Home />} />
           <Route exact path="/start" element={<StartPage />} />
           <Route exact path="/shop" element={<ProductDisplay />} />
@@ -37,8 +46,16 @@ function App() {
           <Route exact path="/testAnswers" element={<CheckAnswers />} />
           <Route exact path="/test" element={<Test />} />
           <Route exact path="/menu" element={<Menu />} />
+          <Route exact path="/ticket" element={<Ticket />} />
+          
         </Routes>
+
+        <SideBar show={showSideBar} />
       </BrowserRouter>
+
+      
+        
+      
     </div>
   );
 }
