97f158e1c5255cd09fb5b4f776fc0fe33acf4d66
rkdmsslasla <gaeun9477@gmail.com>
2023-11-20 00:24:37 +0900

    ProductDetail 프론트엔드

--- client/src/components/views/ShoppingPage/ProductDetail.js
+++ client/src/components/views/ShoppingPage/ProductDetail.js
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 
+import './ProductDetail.css';
+
 function ProductDetail() {
     const navigate = useNavigate();
     const location = useLocation();
@@ -31,20 +33,20 @@ function ProductDetail() {
     return (
         <div className='ProductDetail_background'>
             <div className='ProductDetail_main_container'>
-                <div className="ProductDetial_top_container">
-                    임시이미지
-                </div>
-                <div className="ProductDetial_bottom_container">
+                <div className="ProductDetial_inner_container">
+                    <div className="ProductDetail_img_container">
+                        임시이미지
+                    </div>
                     <div className="ProductDetail_main_description_container">
                         <div className="ProductDetail_description_title_container">
                             {product.title}
                         </div>
                         <div className="ProductDetail_description_price_container">
                             <div className="ProductDetail_description_original_price">
-                                {product.originalPrice}
+                                {product.originalPrice}￦
                             </div>
                             <div className="ProductDetail_description_discounted_price">
-                                {product.discountedPrice}
+                                {product.discountedPrice}￦
                             </div>
                         </div>
                     </div>
@@ -53,21 +55,32 @@ function ProductDetail() {
                             {product.describe}
                         </div>
                         <div className="ProductDetail_sub_description_content_container">
-                            사이즈 : 10cm x 10cm
-                            <br />
-                            소재 : 제품 소재
-                            <br />
-                            제작 : 00기 디자인팀 000
+                            <div className="ProductDetail_sub_description_content_title_container">
+                                Details &gt;
+                            </div>
+                            <div className="ProductDetail_sub_description_content_body_container">
+                                사이즈 : 10cm x 10cm
+                                <br />
+                                소재 : 제품 소재
+                                <br />
+                                제작 : 00기 디자인팀 000
+                            </div>
+
                         </div>
                     </div>
-
                 </div>
 
+                <div className="ProductDetial_bottom_button_wrapper">
+                    <button className="ProductDetail_cart_button" onClick={addToCart}>
+                        장바구니
+                    </button>
+                    <button className="ProductDetail_buy_button" onClick={addToCart}>
+                        구매하기
+                    </button>
+                </div>
+
+
             </div >
-            굿즈상세설명페이지
-            <button className="ProductDetail_cart_button" onClick={addToCart}>
-                장바구니 담기
-            </button>
         </div >
     )
 }
