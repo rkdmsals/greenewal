987aa6ba2c02b4e8a97163bd4f163dac97bbaeda
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-11-21 07:32:26 +0900

    백엔드 라우팅 변경(api/ 추가)

--- client/src/components/views/Feedback/Feedback.js
+++ client/src/components/views/Feedback/Feedback.js
@@ -2,7 +2,7 @@ import "./Feedback.css";
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ReactComponent as Dia } from "../../../assets/images/SpeakerPage/dia.svg";
 import { auth } from '../../../_actions/user_action';
@@ -13,100 +13,104 @@ const Feedback = () => {
     const [Content, setContent] = useState("")
     const [feedbacks, setFeedbacks] = useState([]);
     const [StudentID, setStudentID] = useState("")
+    useEffect(() => {
+        dispatch(auth()).then(response => {
+            console.log(response);
+
+            if (response.payload.isAuth) {
+                setStudentID(response.payload.studentID);
+                console.log(response.payload.studentID);
+                return
+            } else {
+                navigate('/login');
+            }
+        })
+        // axios.post("/api/login", (req, res) => {
+        //     console.log(res)
+        // })
+
+    }, [])
+    useEffect(() => {
+        axios.get('/api/feedback/read')
+            .then(response => {
+                console.log(response.data);
+                setFeedbacks(response.data);
+            });
+    }, []);
 
     const onContentHandler = (event) => {
         setContent(event.currentTarget.value)
     }
     const deleteClick = (e) => {
         console.log(e.target.id)
-        axios.delete('/feedback', { data : { _id : e.target.id } })
-        .then(() => {
-          console.log('delete성공');
-        })
-        .catch(() => {
-          console.log('delete실패');
-        })
-      }
+        axios.delete('/api/feedback/delete', { data: { _id: e.target.id } })
+            .then(() => {
+                console.log('delete성공');
+                window.location.reload()
+            })
+            .catch(() => {
+                console.log('delete실패');
+            })
+    }
     const onSubmitHandler = (event) => {
         //event.preventDefault();
 
-        axios.post('/feedback', {
+        axios.post('/api/feedback/upload', {
             content: Content,
             studentID: StudentID
-          })
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-    }
-    useEffect(() => {
-        axios.get('/feedback')
-            .then(response => {
-                console.log(response.data);
-                setFeedbacks(response.data);
-            });
-    }, []);
-    useEffect(() => {
-        dispatch(auth()).then(response => {
-            console.log(response);
-
-            if(response.payload.isAuth) {
-                setStudentID(response.payload.studentID);
-                console.log(response.payload.studentID);
-                return
-            } else{
-                navigate('/');
-            }
         })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
-
-    }, [])
 
     return (
         <div className="fb_main_container">
             <div className="fb_title_container">
-            <div className="fb_title">Feedback <br /> To Ewhaian <div className="fb_dia_svg"><Dia /></div> </div>
-            <div className="fb_title_content">22주년을 맞이해 새롭게 단장한 이화이언에게 <br /> 하고싶은 말을 들려주세요!</div>
+                <div className="fb_title">Feedback <br /> To Ewhaian <div className="fb_dia_svg"><Dia /></div> </div>
+                <div className="fb_title_content">22주년을 맞이해 새롭게 단장한 이화이언에게 <br /> 하고싶은 말을 들려주세요!</div>
             </div>
 
             <div className="fb_main_content_container">
-            <div className="fb_main_content_inner_container">
-            <div className="fb_main_content_inner_inner_container">
-                <ul className="fb_main_content_list">
-                {feedbacks.map((feedback, i) => (
-                    <li className="fb_main_content" key={feedback._id}>
-                        {feedback.content} <button id = {feedback._id} onClick={deleteClick}> 삭제 </button>
-                    </li>
-                ))}
-                </ul>
+                <div className="fb_main_content_inner_container">
+                    <div className="fb_main_content_inner_inner_container">
+                        <ul className="fb_main_content_list">
+                            {feedbacks ? feedbacks.map((feedback, i) => (
+                                <li className="fb_main_content" key={feedback._id}>
+                                    {feedback.content} <button id={feedback._id} onClick={deleteClick}> 삭제 </button>
+                                </li>
+                            )) : <div></div>}
+                        </ul>
+                    </div>
+
+
+                    <div className="add_feedback_container">
+                        <form className='add_feedback'
+                            onSubmit={onSubmitHandler}
+                        >
+                            <div className='fb_content_input_container'>
+                                <div className='fb_content_inputset'>
+
+                                    <input className="fb_content_input" type="text" minLength="5" maxLength="300"
+                                        placeholder="피드백을 입력해 주세요. (5-300자)"
+                                        value={Content} onChange={onContentHandler} />
+                                    <button className="btn_add_fb" type='submit'>
+                                        <img className="fb_btn_img" src="/img/Feedback/button.png" />
+                                    </button>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
             </div>
-            
-             
-        <div className="add_feedback_container">
-        <form className='add_feedback'
-            onSubmit={onSubmitHandler}
-        >
-            <div className='fb_content_input_container'>
-            <div className='fb_content_inputset'>
-            
-           <input className="fb_content_input" type="text" minlength="5" maxlength="300"
-           placeholder="피드백을 입력해 주세요. (5-300자)"
-           value={Content} onChange={onContentHandler} />
-           <button className="btn_add_fb" type='submit'>
-               <img className="fb_btn_img" src="/img/Feedback/button.png" />
-           </button>
-            </div>
-            </div>
-           
-       </form>
-       </div>
-       </div>
-       </div>
-        
+
         </div>
-        
+
     );
 }
 
