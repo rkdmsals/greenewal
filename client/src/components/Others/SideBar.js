40d086b147b48ac9d9ac7b7ad20d672a0b3e13af
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-11-21 18:47:35 +0900

    먹거리 부스 라우팅, 사이드바 라우팅 및 css 수정,
    로그인 시에만 모바일 티켓 확인하고 유저 정보 보여주도록 수정함,

--- client/src/components/Others/SideBar.js
+++ client/src/components/Others/SideBar.js
@@ -9,15 +9,16 @@ function SideBar({ show }) {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [showSubMenu, setShowSubMenu] = useState(false)
-    const [userName, setUserName] = useState("김이화")
+    const [userName, setUserName] = useState("")
     // const [showSideBar, setShowSideBar] = useState({ show })
 
     useEffect(() => {
         dispatch(auth()).then(response => {
             if (!response.payload.isAuth) {
-                return 
-            } else { console.log(response.payload.name)
-            setUserName(response.payload.name);
+                return;
+            } else {
+                // console.log(response.payload.name)
+                setUserName(response.payload.name);
             }
         })
     })
@@ -26,44 +27,46 @@ function SideBar({ show }) {
         <div className={show ? 'sidebar active' : 'sidebar'} >
             <div className="SideBar_inner">
                 <div className="SideBar_inner_header">
-                    <div className="hello">
+                    {userName ? <div className="hello">
                         <div> 안녕하세요, </div>
                         <div className="hello_name">
                             <div> {userName} 님</div>
                             <img src="/img/SideBar/ewhaian_icon.png" alt="icon" className="icon" />
+
                         </div>
-                    </div>
+                    </div> : <div className="hello"><div className="NavLogin" onClick={() => navigate("/login")}>로그인</div></div>}
+
                     <div className="border"></div>
                 </div>
-                <div className="sidebar_ticket" onClick={() => navigate('/ticket')}>
+                {userName ? <div className="sidebar_ticket" onClick={() => navigate('/ticket')}>
                     <div className="ticket_title">
                         <img src="/img/SideBar/ticket_icon.png" alt="ticket" className="ticket_icon" />
                         <div>나의 티켓</div>
                     </div>
                     <img src="/img/SideBar/ticket_pic.png" alt="ticket" className="ticket_pic" />
-                </div>
+                </div> : <div></div>}
                 <ul>
                     <li>
-                        <Link to="/">행사 정보</Link>
+                        <Link to="/eventinfo">행사 정보</Link>
                     </li>
                     <li className="booth">
                         <div className="booth_tap" onClick={() => setShowSubMenu(!showSubMenu)}>부스 정보</div>
                         <div className={showSubMenu ? 'submenu active' : 'submenu'}>
-                            <Link className="sub" to="/login">네컷사진 부스</Link>
-                            <Link className="sub" to="/login">퀴즈,게임 부스</Link>
-                            <Link className="sub" to="/login">먹거리 부스</Link>
-                            <Link className="sub" to="/login">리뉴얼 부스</Link>
-                            <Link className="sub" to="/login">운영진 부스</Link>
+                            <Link className="sub" to="/photo">네컷사진 부스</Link>
+                            <Link className="sub" to="/quizgame">퀴즈,게임 부스</Link>
+                            <Link className="sub" to="/foods">먹거리 부스</Link>
+                            <Link className="sub" to="/feedback">리뉴얼 부스</Link>
+                            {/* <Link className="sub" to="/login">운영진 부스</Link> */}
                         </div>
                     </li>
                     <li>
-                        <Link to="/login">강연 정보</Link>
+                        <Link to="/speaker">강연 정보</Link>
                     </li>
                     <li>
-                        <Link to="/register">이벤트 정보</Link>
+                        <Link to="/event">이벤트 정보</Link>
                     </li>
                     <li>
-                        <Link to="/">지난 굿즈 상점</Link>
+                        <Link to="/shop">지난 굿즈 상점</Link>
                     </li>
                 </ul>
 
