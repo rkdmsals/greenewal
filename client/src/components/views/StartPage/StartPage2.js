64586fece95865c41019ebd031c7ed9bd6170934
MK-cyb <mindy50@ewhain.net>
2023-11-23 01:02:32 +0900

    greenewal page 1차

--- client/src/components/views/StartPage/StartPage2.js
+++ client/src/components/views/StartPage/StartPage2.js
@@ -1,31 +1,80 @@
 import { useNavigate } from "react-router-dom";
-import StartPage from "./StartPage";
+import Slider from "react-slick";
 import "./StartPage2.css"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 function StartPage2() {
     const navigate = useNavigate();
+    const redirectToExternalLink = (link) => {
+        // 인자로 받은 링크로 이동
+        window.location.href = link;
+      };
+      const handleClick = (path) => {
+        // 인자로 받은 경로로 페이지 이동
+        navigate(path);
+      };
+    //settings 부분, 슬라이더의 기능을 조정할 수 있다.
+    const settings = {
+        dots: true,
+        infinite: true,
+        //fade: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true
+    }
+    
 
-    // setTimeout(() => {
-    //     $(".startPage_background").css("display", "none")
-    // }, 3000)
-
-    return (<div className="GrnPage">
-        {/* <StartPage /> */}
-        <div>
-            <div className="GrnTitle">Ewahaian</div>
+    return (
+        <div className="GrnPage">
+        <div className="GrnPage_Title_container">
+            <div className="Grn_img_container">
+                <div className="GrnTitle">Ewahaian</div>
+                <img className="Grn_clober_green" src="/img/StartPage/clober_green.png" />
+            </div>
             <div className="GrnTitle">Gree<span>n-ew</span>al</div>
             <img></img>
         </div>
-        <div className="GrnTxt">이화이언 리뉴얼을 기념하여 11/27 {"<Green-ewal>"}행사가 이삼봉홀에서 진행됩니다.</div>
+        <div className="GrnTxt">이화이언 리뉴얼을 기념하여 <br />11/27 {"<Green-ewal>"}행사가 <br />이삼봉홀에서 진행됩니다.</div>
         <div className="GrnBanner">
-            <img className="GrnBanE" onClick={() => navigate("https://www.notion.so/ewhaianweb/11-00bd59c9c1aa4795bd2edfb293e67d8b")}></img>
+        <Slider {...settings}>
+            <div>
+                <img className="GrnBanE" onClick={() => redirectToExternalLink('https://naver.com')}></img>
+            </div>
+            <div>
+                <img className="GrnBanE" id="GrnBanE_2" onClick={() => redirectToExternalLink('https://naver.com')}></img>
+            </div>
+        </Slider>
         </div>
         <div className="GrnRoute">
-            <div>행사 정보<img></img></div>
-            <div>부스 정보<img></img></div>
-            <div>강연 정보<img></img></div>
-            <div>이벤트 정보<img></img></div>
+            <div className="GrnRoute_inner_container">
+            <div className="GrnRoute_menu_container">
+                <div className="GrnRoute_menu">행사 정보</div>
+                <img className="Grn_arr_black" src="/img/StartPage/arr_black.png" onClick={() => handleClick('/eventinfo')}></img>
+            </div>
+            <div className="Grn_line"></div>
+            <div className="GrnRoute_menu_container">
+                <div className="GrnRoute_menu">부스 정보</div>
+                <img className="Grn_arr_black" src="/img/StartPage/arr_black.png" onClick={() => handleClick('/booth')}></img>
+            </div>
+            <div className="Grn_line"></div>
+            <div className="GrnRoute_menu_container">
+                <div className="GrnRoute_menu">강연 정보</div>
+                <img className="Grn_arr_black" src="/img/StartPage/arr_black.png" onClick={() => handleClick('/speaker')}></img>
+            </div>
+            <div className="Grn_line"></div>
+            <div className="GrnRoute_menu_container">
+                <div className="GrnRoute_menu">이벤트 정보</div>
+                <img className="Grn_arr_black" src="/img/StartPage/arr_black.png" onClick={() => handleClick('/event')}></img>
+            </div>
+            <div className="Grn_line"></div>
+            </div>
         </div>
-    </div>)
+    </div>
+    )
 }
 
 export default StartPage2;
