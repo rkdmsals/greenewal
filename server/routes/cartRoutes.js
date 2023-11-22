fae2b02e01b968a029e64874a018e98d16a19a37
MK-cyb <mindy50@ewhain.net>
2023-11-22 17:46:56 +0900

    장바구니 추가 post

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cartRouter = express.Router();
 const cartController = require('../controllers/cartController');
 
-cartRouter.post('/api/addToCart', cartController.addToCart);
+cartRouter.post('/add', cartController.addToCart);
 // cartRouter.get('/api/cart/:userId', cartController.getCart);
 
 module.exports = cartRouter;
