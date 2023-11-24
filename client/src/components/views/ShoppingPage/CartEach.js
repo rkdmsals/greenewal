f25b27f535dba1f06e1cd620610f6791f05a08f5
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-11-24 19:13:53 +0900

    장바구니 수량 조절 기능

--- client/src/components/views/ShoppingPage/CartEach.js
+++ client/src/components/views/ShoppingPage/CartEach.js
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 function CartEach(values) {
     const userId = values.userId;
     const productId = values.productId;
-    const quantity = values.quantity;
+    // const quantity = values.quantity;
     const productName = values.productName;
     const price = values.price;
+    const [quantity, setQuantity] = useState(values.quantity);
+    // useEffect(() => {
+    //     setQuantityRe(quantity)
+    // }, [quantity])
+    const minusQuantity = () => {
+
+        if (quantity > 1) {
+            const reQuantity = quantity - 1
+            axios.post("/api/addToCart/modify", {
+                userId: userId,
+                productId: productId,
+                quantity: reQuantity,
+            })
+            return setQuantity(reQuantity)
+        }
+
+    }
+    const plusQuantity = () => {
+
+        if (quantity < 10) {
+            const reQuantity = quantity + 1
+            axios.post("/api/addToCart/modify", {
+                userId: userId,
+                productId: productId,
+                quantity: reQuantity,
+            })
+            return setQuantity(reQuantity)
+        }
+    }
     return (
         <div className='CartEach'>
             <img className="CartProductImg"></img>
@@ -15,9 +46,9 @@ function CartEach(values) {
                 <div className="CartEachButtons">
                     <div className="DelBtn"></div>
                     <div className="CartNum">
-                        <div className="MinusBtn"></div>
+                        <div className="MinusBtn" onClick={minusQuantity}></div>
                         {quantity}
-                        <div className="PlusBtn"></div>
+                        <div className="PlusBtn" onClick={plusQuantity}></div>
                     </div>
                 </div>
             </div>
