4f2984667941317595957a453d594f550ccb9a8a
jenjeulim <128949350+jenjeulim@users.noreply.github.com>
2023-11-24 23:12:39 +0900

    이벤트 페이지 수정

--- client/src/components/views/menu/menu.js
+++ client/src/components/views/menu/menu.js
@@ -7,7 +7,8 @@ const Menu = () => {
       id: 1,
       image: '/img/menu/bp.png',
       text: '미니 붕어빵',
-      pricetext: '2,500₩',
+      downtext: '미니 붕어빵(5개)',
+      pricetext: '3,000₩',
       contenttext: (
         <div style={{ whiteSpace: 'pre-line' }}>
           겨울엔 붕어빵!<br /> 따뜻한 미니 붕어빵 든든하게 먹고 수업 들으러~
@@ -19,7 +20,8 @@ const Menu = () => {
       id: 2,
       image: '/img/menu/dm.png',
       text: '델리만쥬',
-      pricetext: '2500₩',
+      downtext: '델리만쥬(5개)',
+      pricetext: '3,000₩',
       contenttext: (
         <div style={{ whiteSpace: 'pre-line' }}>
           휴게소, 지하철에서의 추억이 새록새록~<br />커스타드 크림이 들어있는 따뜻한 델리만쥬!
@@ -30,7 +32,8 @@ const Menu = () => {
       id: 3,
       image: '/img/menu/hc.png',
       text: '핫초코',
-      pricetext: '2500₩',
+      downtext: '핫초코',
+      pricetext: '2,500₩',
       contenttext:'추운 겨울, 따뜻한 핫초코 한 잔으로 몸을 따뜻하게!',
     }
   ]);
@@ -61,7 +64,7 @@ const Menu = () => {
       <div className="selected-item">
         <img src={selectedItem.image} alt={selectedItem.text} className="selected-item-img" />
         <div className="items">
-          <span className="selected-item-text">{selectedItem.text}</span> 
+          <span className="selected-item-text">{selectedItem.downtext}</span> 
           <span className="selected-item-pricetext">{selectedItem.pricetext}</span>
         </div>
         <div className="selected-item-contenttext">{selectedItem.contenttext}</div>
