d461c4bc268a77dc787006205e17da2b72285c86
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-11-26 04:37:24 +0900

    티켓 페이지 인증 - 일부

--- client/src/components/views/Ticket/TicketPage.js
+++ client/src/components/views/Ticket/TicketPage.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./TicketPage.css";
-//import { useState } from "react";
-//import { auth } from '../../../_actions/user_action';
-//import { useDispatch } from 'react-redux';
-//import { useEffect } from 'react';
+import { auth } from '../../../_actions/user_action';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import { ReactComponent as Dia } from "../../../assets/images/SpeakerPage/dia.svg";
 import { useNavigate } from 'react-router-dom';
-
+import { useLocation } from 'react-router-dom';
 function TicketPage() {
-
+    const dispatch = useDispatch();
+    const location = useLocation;
     const navigate = useNavigate();
+    const [isTicket, setIsTicket] = useState(false);
+    if (location.state.isTicket) { setIsTicket(location.state.isTicket); }
 
-    return(
-        <div className='TicketPage_background'>
-            <div className='TicketPage_title'>
-            <div> Ticket </div>
-            <div className='dia'><Dia /></div>
+    //여기서 비번 입력하면 isAuth 설정되도록!!
+    //비번은 음.."RMFLSBDJF1127"
+    console.log(isTicket)
+    return (<div>
+        <div className={isTicket ? "Show" : "DontShow"}>
+            <div className='CheckTicket'></div>
+            <div className='TicketPage_background'>
+                <div className='TicketPage_title'>
+                    <div> Ticket </div>
+                    <div className='dia'><Dia /></div>
+                </div>
+                <img src="/img/Ticket/ticket.png" alt='ticket' className='ticket' />
+                <img src='/img/Ticket/nav_speaker.png' alt='nav_speaker' className='nav_speaker'
+                    onClick={() => navigate("/speaker")}
+                />
             </div>
-           <img src="/img/Ticket/ticket.png" alt='ticket' className='ticket' />
-           <img src='/img/Ticket/nav_speaker.png' alt='nav_speaker' className='nav_speaker'
-           onClick={() =>navigate("/speaker")} 
-           />
-
         </div>
+    </div>
+
     )
 }
 
