44eaff620a76146e1e68821379a2eb5945d822fb
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-11-26 22:47:48 +0900

    주문 확인 수정

--- client/src/components/views/ShoppingPage/OrderCheck.js
+++ client/src/components/views/ShoppingPage/OrderCheck.js
@@ -32,9 +32,9 @@ function OrderCheck() {
         if (userId) {
             axios.get(`/api/addToCart/checkOrder/${userId}`)
                 .then(response => {
-                    // console.log(response.data)
-                    if (response.data) {
-                        return setOrderThings(response.data);
+                    console.log(response.data.order)
+                    if (response.data.order) {
+                        return setOrderThings(response.data.order);
                     } else {
                         return alert("주문 내역이 없습니다!")
                     }
